b912dac021d1b56919efb00461af4a0186f626d1
gmargo <gmargo@mit.edu>
2023-01-05 07:54:39 -0500

    Update dependencies

--- setup.py
+++ setup.py
@@ -9,12 +9,13 @@ setup(
     packages=find_packages(),
     author_email='gmargo@mit.edu',
     description='Toolkit for deployment of sim-to-real RL on the Unitree Go1.',
-    install_requires=['ml_logger==0.8.71',
+    install_requires=['ml_logger==0.8.117',
                       'ml_dash==0.3.20',
-                      'jaynes==0.8.11',
-                      'params-proto==2.9.6',
-                      'gym',
+                      'jaynes==0.9.2',
+                      'params-proto==2.10.5',
+                      'gym==0.14.0',
                       'tqdm',
                       'matplotlib',
+                      'numpy==1.23.5'
                       ]
 )
